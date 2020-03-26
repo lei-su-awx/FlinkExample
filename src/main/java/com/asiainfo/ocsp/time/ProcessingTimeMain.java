878e7cb928031fcb5772393160691f6c1ee148a5
sulei5 <sulei5@asiainfo.com>
2020-03-26 17:08:46 +0800

    格式化代码

--- src/main/java/com/asiainfo/ocsp/time/ProcessingTimeMain.java
+++ src/main/java/com/asiainfo/ocsp/time/ProcessingTimeMain.java
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.*;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.table.descriptors.Csv;
+import org.apache.flink.table.descriptors.FormatDescriptor;
 import org.apache.flink.table.descriptors.Kafka;
 import org.apache.flink.table.descriptors.Schema;
 import org.apache.flink.types.Row;
@@ -24,35 +25,32 @@ public class ProcessingTimeMain {
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
         see.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
         see.setParallelism(1);
-        EnvironmentSettings es = EnvironmentSettings.newInstance().inStreamingMode().build();
+        EnvironmentSettings es = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
         StreamTableEnvironment ste = StreamTableEnvironment.create(see, es);
 
         Properties properties = new Properties();
         properties.setProperty("group.id", "sulei");
         properties.setProperty("bootstrap.servers", "10.1.236.139:6667,10.1.236.143:6667,10.1.236.78:6667");
 
-        Kafka inputKafka = new Kafka().properties(properties).version("010").topic("sulei_in");
+        Kafka inputKafka = new Kafka().properties(properties).version("universal").topic("sulei_in");
         Schema inputSchema = new Schema()
+                .field("proctime", DataTypes.TIMESTAMP()).proctime()
                 .field("stringg", DataTypes.STRING())
                 .field("intt", DataTypes.INT())
                 .field("floatt", DataTypes.FLOAT())
                 .field("doublee", DataTypes.DOUBLE())
-                .field("longg", DataTypes.BIGINT())
-                .field("proctime", DataTypes.TIMESTAMP()).proctime();
-        Csv csv = new Csv()
-                .fieldDelimiter(',')
-                .ignoreParseErrors();
+                .field("longg", DataTypes.BIGINT());
+        FormatDescriptor csv = new Csv().ignoreParseErrors();
 
         ste.connect(inputKafka)
-                .withSchema(inputSchema)
                 .withFormat(csv)
-                .inAppendMode()
+                .withSchema(inputSchema)
                 .createTemporaryTable("input");
 
         Table input = ste.from("input");
         GroupWindowedTable window = input.window(Tumble.over("30.second").on("proctime").as("tt"));
         WindowGroupedTable group = window.groupBy("stringg, tt");
-        Table result = group.select("stringg, sum(intt) as s2, sum(floatt) as s3, sum(doublee) as s4, sum(longg) as s5, tt.start");
+        Table result = group.select("tt.start as start, stringg, intt.sum as s2, floatt.sum as s3, doublee.sum as s4, longg.sum as s5");
 
         ste.toAppendStream(result, TypeInformation.of(Row.class)).print();
 
--- src/main/java/com/asiainfo/ocsp/types/TypesMain.java
+++ src/main/java/com/asiainfo/ocsp/types/TypesMain.java
@@ -5,6 +5,7 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.formats.csv.CsvRowDeserializationSchema;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.KafkaTableSource;
 import org.apache.flink.table.api.*;
@@ -25,6 +26,7 @@ public class TypesMain {
 
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+        see.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
         EnvironmentSettings es = EnvironmentSettings.newInstance().inStreamingMode().build();
         StreamTableEnvironment ste = StreamTableEnvironment.create(see, es);
 
@@ -42,7 +44,7 @@ public class TypesMain {
         Table source = ste.fromTableSource(kafkaSource);
 
         // Group
-        GroupWindowedTable windowTable = source.window(Tumble.over("").on("").as(""));
+        GroupWindowedTable windowTable = source.window(Tumble.over("30").on("").as(""));
         WindowGroupedTable groupedTable = windowTable.groupBy("1");
         Table finalTable = groupedTable.select("1, avg(2) as c2, avg(3) as c3, avg(4) as c4, avg(5) as c5");
 
