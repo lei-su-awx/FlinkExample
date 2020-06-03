68d7ff8f72eb251ae16707c213d0041e2aee23f5
sulei5 <sulei5@asiainfo.com>
2020-06-03 09:07:36 +0800

    去掉company相关

--- src/main/scala/com/asiainfo/ocsp/TableAndProperties.scala
+++
@@ -1,8 +0,0 @@
-package com.asiainfo.ocsp
-
-import org.apache.flink.table.api.Table
-
-import scala.collection.mutable
-
-class TableAndProperties(val table: Table, val props: mutable.LinkedHashMap[String, String]) {
-}
